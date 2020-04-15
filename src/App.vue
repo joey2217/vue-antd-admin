c09ae427d69e79c57cf961e9bc1fb76bd727dc00
BurNing1993 <786678257@qq.com>
2020-04-15 11:44:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+ <a-button type="primary">Primary</a-button>
+    <a-button>Default</a-button>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    a: [
+      'a',
+      'd',
+    ],
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+});
