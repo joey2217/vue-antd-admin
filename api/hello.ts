e9b6c0f2e2886331028f055b0e0a39858b28b0fc
Joey <786678257@qq.com>
2021-01-14 18:28:33 +0800

    chore:api

--- api/hello.ts
+++ api/hello.ts
@@ -3,6 +3,6 @@ import { NowRequest, NowResponse } from '@vercel/node';
 export default (request: NowRequest, response: NowResponse) => {
   response.status(200).json({
     requestStr: JSON.stringify(request),
-    responseStr: JSON.stringify(response),
+    request,
   });
 };
